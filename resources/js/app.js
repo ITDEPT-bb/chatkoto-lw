926db87ee795efd18f5b7012a780320bfc872c5e
ITDEPT-bb <itdept.bb88@gmail.com>
2025-07-24 14:48:03 +0800

    chore: testing hotfix

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+
+import { createLaravelVitePlugin } from 'laravel-vite-plugin'
+import 'laravel-vite-plugin/plugins/livewire'
+import { Livewire } from 'livewire'
+
+Livewire.start()
