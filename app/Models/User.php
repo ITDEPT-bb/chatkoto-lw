5c89e823770f960e99ab42fdbf3481dbdd2d451d
ITDEPT-bb <itdept.bb88@gmail.com>
2025-07-24 13:43:54 +0800

    feat: installing laravel breeze fo auth and making the chat

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Namu\WireChat\Traits\Chatable;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+
+    use Chatable;
 
     /**
      * The attributes that are mass assignable.
@@ -44,5 +47,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function canCreateChats(): bool
+    {
+        return $this->hasVerifiedEmail();
+    }
+
+    public function canCreateGroups(): bool
+    {
+        return $this->hasVerifiedEmail() === true;
     }
 }
